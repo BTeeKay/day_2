051e425d7e31e9cdac92c89154ec1c50d40f5b27
Brian Kerr <briantkerr91@gmail.com>
2022-05-03 10:46:40 +0100

    testing pushing again, added some stuff

---
+++ day2.py
@@ -0,0 +1,19 @@
+#testing testing testing
+if 4 == 2 + 2:
+    print("True")
+
+if "cat" == "cat":
+    print("True")
+
+if "cat" == "dog":
+    print("True")
+else:
+    print("False")
+
+if str(1) == "1":
+    print("True")
+else:
+    print("False")
+
+if 5 > 4:
+    print("True")
